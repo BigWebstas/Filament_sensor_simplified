04a4b3495f7bba6b6807f92d7201c9400d856c2a
Joshua Webster <2896329+BigWebstas@users.noreply.github.com>
2022-02-07 10:35:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Lukáš Malatinský - Updated by bigWebstas"
 plugin_author_email = "luckyx182@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/luckyx182/Filament_sensor_simplified"
+plugin_url = "https://github.com/BigWebstas/Filament_sensor_simplified"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
